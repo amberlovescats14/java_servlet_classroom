3eb5483581284b9a3c8a1a92bb37664ceec47672
Amber Jones <amberlovescats14@gmail.com>
2020-01-13 11:33:11 -0600

    changed method to a doPost

--- src/main/java/controllers/access/AdminServlet.java
+++ src/main/java/controllers/access/AdminServlet.java
@@ -9,19 +9,21 @@ import java.io.IOException;
 
 @WebServlet(name = "AdminServlet", urlPatterns = "/access/admin")
 public class AdminServlet extends HttpServlet {
+//    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//
+//        // if not logged in, this will re direct REJECT
+//
+//    }
+
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-        // if not logged in, this will re direct REJECT
-
-    }
-
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         //get master code
         String sessionCode = (String)req.getSession().getAttribute("access_code");
         // get form code
         String requestCode = req.getParameter("code");
 
+        System.out.println(sessionCode);
+        System.out.println(requestCode);
 
         if(sessionCode == null || requestCode == null){
             req.getRequestDispatcher("/WEB-INF/sessions/rejected.jsp").forward(req,res);
